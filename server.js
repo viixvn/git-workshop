a53392c35c91deaee9947a491341a3aadc8fc5f2
Nomin-Erdene <nomin-erdene@berkeley.edu>
2025-02-13 19:44:44 -0800

    created our server

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello world!</h1>');
+});
+
+app.get('/about', (req, res) => {
+    res.send('<h1>About page<h1/><p>Its me!<p/>');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
